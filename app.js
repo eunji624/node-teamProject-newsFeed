ec91c698dcbf5cfde4248bba74fa8ad2b4b55218
eunji624 <allrkatk@naver.com>
2023-11-22 19:17:05 +0900

    [update] : 회원기능 구현

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
+const cookieParser = require('cookie-parser');
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+const UsersRouter = require('./routers/users.router.js');
+app.use('/api', UsersRouter);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
