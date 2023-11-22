0c072fefb5ea54c1e3f89bf962c844b53b505120
eunji624 <allrkatk@naver.com>
2023-11-22 20:06:58 +0900

    [test] : 미들웨어 테스트

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.use(express.json());
 app.use(cookieParser());
 
 const UsersRouter = require('./routers/users.router.js');
+const PostsRouter = require('./routers/posts.router.js');
+const CommentsRouter = require('./routers/comment.router.js');
+
 app.use('/api', UsersRouter);
+app.use('/api', PostsRouter);
+app.use('/api', CommentsRouter);
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
--- routers/comment.router.js
+++ routers/comment.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authMiddleware = require('../middlewares/need-signin.middlware');
+const { authMiddleware } = require('../middleware/auth.js');
 const router = express.Router();
 const { Posts, Users, Comments } = require('../models');
 require('dotenv').config();
@@ -32,7 +32,14 @@ router.get('/post/:postId', async (req, res) => {
 		where: {
 			id: postId,
 		},
-		attributes: ['id', 'category', 'title', 'content', 'petName', 'createdAt'],
+		attributes: [
+			'id',
+			'category',
+			'title',
+			'content',
+			'petName',
+			'createdAt',
+		],
 	});
 	const commentsList = await Comments.findAll({
 		where: { postId: postId },
@@ -51,67 +58,106 @@ router.get('/post/:postId', async (req, res) => {
 
 // //  U Update
 
-router.patch('/comment/:commentId', authMiddleware, async (req, res) => {
-	const { commentId } = req.params;
-	const selectedComment = await Comments.findOne({
-		where: {
-			commentId: commentId,
-		},
-	});
-
-	let { content } = req.body;
-	if (!content) {
-		return res.status(400).json({ success: false, message: '값을 입력하시오' });
-	}
-	if (content === selectedComment.content) {
-		// 409 Conflict  값들끼리 충돌 날 때
-		return res.status(409).json({ success: false, message: '등록된 값이랑 똑같아요' });
-	}
-	if (selectedComment.userId === res.locals.user.id) {
-		await Comments.update(
-			{
-				content: content,
-			},
-			{
-				where: {
-					commentId: commentId,
-				},
-			},
-		);
-		const updatedComment = await Comments.findOne({
+router.patch(
+	'/comment/:commentId',
+	authMiddleware,
+	async (req, res) => {
+		const { commentId } = req.params;
+		const selectedComment = await Comments.findOne({
 			where: {
 				commentId: commentId,
 			},
 		});
-		res.status(200).json({ success: true, message: '댓글이 성공적으로 수정됬습니다', updatedComment });
-	} else {
-		return res.status(403).json({ success: false, message: 'payload의 id와 선택한 댓글 userId가 다릅니다' });
-	}
-});
+
+		let { content } = req.body;
+		if (!content) {
+			return res
+				.status(400)
+				.json({ success: false, message: '값을 입력하시오' });
+		}
+		if (content === selectedComment.content) {
+			// 409 Conflict  값들끼리 충돌 날 때
+			return res
+				.status(409)
+				.json({ success: false, message: '등록된 값이랑 똑같아요' });
+		}
+		if (selectedComment.userId === res.locals.user.id) {
+			await Comments.update(
+				{
+					content: content,
+				},
+				{
+					where: {
+						commentId: commentId,
+					},
+				},
+			);
+			const updatedComment = await Comments.findOne({
+				where: {
+					commentId: commentId,
+				},
+			});
+			res
+				.status(200)
+				.json({
+					success: true,
+					message: '댓글이 성공적으로 수정됬습니다',
+					updatedComment,
+				});
+		} else {
+			return res
+				.status(403)
+				.json({
+					success: false,
+					message: 'payload의 id와 선택한 댓글 userId가 다릅니다',
+				});
+		}
+	},
+);
 // //  U Update
 
 // D delete
 // 미들웨어로 검증 후 payload에 user[id] 추출
-router.delete('/comment/:commentId', authMiddleware, async (req, res) => {
-	const { commentId } = req.params;
-	const selectedComment = await Comments.findOne({
-		where: {
-			commentId: commentId,
-		},
-	});
-	if (!selectedComment) {
-		// 404 not found
-		return res.status(404).json({ success: false, message: '선택한 댓글이 존재하지 않습니다' });
-	}
-	const deletedComment = selectedComment;
-	if (selectedComment.userId === res.locals.user.id) {
-		selectedComment.destroy();
-		return res.status(200).json({ success: true, message: '삭제가 성공적으로 이루어졌습니다', deletedComment });
-	} else {
-		// 403 Forbidden  권한이 없을 때 사용
-		return res.status(403).json({ success: false, message: 'payload의 id와 선택한 댓글 userId가 다릅니다' });
-	}
-});
+router.delete(
+	'/comment/:commentId',
+	authMiddleware,
+	async (req, res) => {
+		const { commentId } = req.params;
+		const selectedComment = await Comments.findOne({
+			where: {
+				commentId: commentId,
+			},
+		});
+		if (!selectedComment) {
+			// 404 not found
+			return res
+				.status(404)
+				.json({
+					success: false,
+					message: '선택한 댓글이 존재하지 않습니다',
+				});
+		}
+		const deletedComment = selectedComment;
+		if (selectedComment.userId === res.locals.user.id) {
+			selectedComment.destroy();
+			return res
+				.status(200)
+				.json({
+					success: true,
+					message: '삭제가 성공적으로 이루어졌습니다',
+					deletedComment,
+				});
+		} else {
+			// 403 Forbidden  권한이 없을 때 사용
+			return res
+				.status(403)
+				.json({
+					success: false,
+					message: 'payload의 id와 선택한 댓글 userId가 다릅니다',
+				});
+		}
+	},
+);
 // // D delete
 
 module.exports = router;
