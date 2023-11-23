8233cfe2707b1e4a2f7cf203ad719f4c21d72e54
leesin1040 <leesin1040@gmail.com>
2023-11-23 20:36:00 +0900

    [add]:css파일 분할 및 ejs정리중

--- app.js
+++ app.js
@@ -8,21 +8,19 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 const path = require('path');
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
 
 const UsersRouter = require('./routers/users.router.js');
 const PostsRouter = require('./routers/posts.router.js');
 const CommentsRouter = require('./routers/comment.router.js');
 
-app.use('/api', UsersRouter);
-app.use('/api', PostsRouter);
-app.use('/api', CommentsRouter);
+app.use('/api', [UsersRouter, PostsRouter, CommentsRouter]);
+// app.use('/api', PostsRouter);
+// app.use('/api', CommentsRouter);
 
 app.get('/', (req, res) => {
 	res.render('main', { title: '팻' });
