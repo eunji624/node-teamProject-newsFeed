f5d33fffd2dc0667dd49ee9232db0a6811cec833
Han9526 <jk2tmdwns@naver.com>
2023-11-27 11:26:26 +0900

    [fix]: error status

--- routers/comment.router.js
+++ routers/comment.router.js
@@ -12,6 +12,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 // C created
+
 router.post(
 	'/comment/:postId',
 	authMiddleware,
@@ -19,19 +20,26 @@ router.post(
 	async (req, res) => {
 		const { content } = req.body;
 		const { postId } = req.params;
-		const createdProduct = await Comments.create({
-			userId: res.locals.user.id,
-			postId: postId,
-			content,
-		});
-		return res.redirect(`/api/post/${postId}`); //미쳤다
+		try {
+			await Comments.create({
+				userId: res.locals.user.id,
+				postId: postId,
+				content,
+			});
+			return res.redirect(`/api/post/${postId}`);
+		} catch (err) {
+			return res.status(500).render('blank', {
+				message:
+					'댓글 등록에 실패하였습니다. 관리자에게 문의 하십시오',
+			});
+		}
 	},
 );
+
 // C created
 
 // R Read
 
-// 댓글 목록 조회
 router.get('/post/:postId', async (req, res) => {
 	const { postId } = req.params;
 	const postsDetail = await Posts.findOne({
@@ -63,7 +71,6 @@ router.get('/post/:postId', async (req, res) => {
 		},
 		order: [['updatedAt', 'DESC']],
 	});
-	// ~~
 	if (postsDetail) {
 		try {
 			if (!req.cookies.Authorization) {
@@ -84,13 +91,17 @@ router.get('/post/:postId', async (req, res) => {
 				});
 			}
 		} catch (err) {
-			console.log(err);
+			return res.status(500).render('blank', {
+				message: '관리자에게 문의 하십시오',
+			});
 		}
 	}
 });
+
 // R Read
 
-// //  U Update
+// U Update
+
 router.patch(
 	'/comment/:commentId',
 	authMiddleware,
@@ -117,63 +128,75 @@ router.patch(
 					},
 				);
 				res.redirect(`/api/post/${selectedComment.postId}`);
+			} else {
+				return res.status(403).render('blank', {
+					message: '댓글 수정에 실패했습니다.',
+				});
 			}
 		} catch (err) {
-			console.log(err);
+			return res.status(500).render('blank', {
+				message: '댓글 수정에 실패했습니다. 관리자에게 문의 하십시오',
+			});
 		}
 	},
 );
+
 // //  U Update
 
 // D delete
-// 미들웨어로 검증 후 payload에 user[id] 추출
+
 router.delete(
 	'/comment/:commentId',
 	authMiddleware,
 	passwordValidator,
 	async (req, res) => {
-		const { commentId } = req.params;
-		const { password } = req.body;
-		const selectedComment = await Comments.findOne({
-			where: {
-				commentId: commentId,
-			},
-		});
-		const checkedPaswword = await Users.findOne({
-			where: {
-				id: selectedComment.userId,
-			},
-		});
-		const isSame = await bcrypt.compare(
-			password,
-			checkedPaswword.password,
-		);
-		if (!isSame) {
-			return res.render('blank', {
-				message: '입력하신 비밀번호가 올바르지 않습니다.',
+		try {
+			const { commentId } = req.params;
+			const { password } = req.body;
+			const selectedComment = await Comments.findOne({
+				where: {
+					commentId: commentId,
+				},
 			});
-		}
+			const checkedPaswword = await Users.findOne({
+				where: {
+					id: selectedComment.userId,
+				},
+			});
+			const isSame = await bcrypt.compare(
+				password,
+				checkedPaswword.password,
+			);
+			if (!isSame) {
+				return res.status(403).render('blank', {
+					message: '입력하신 비밀번호가 올바르지 않습니다.',
+				});
+			}
 
-		if (!selectedComment) {
-			// 404 not found
-			return res.status(404).json({
-				success: false,
-				message: '선택한 댓글이 존재하지 않습니다',
-			});
-		}
-		const deletedComment = selectedComment;
-		if (selectedComment.userId === res.locals.user.id) {
-			selectedComment.destroy();
-			return res.redirect(`/api/post/${selectedComment.postId}`);
-		} else {
-			// 403 Forbidden  권한이 없을 때 사용
-			return res.status(403).json({
-				success: false,
-				message: 'payload의 id와 선택한 댓글 userId가 다릅니다',
+			if (!selectedComment) {
+				// 404 not found
+				return res.status(404).render('blank', {
+					message: '이미 삭제된 댓글입니다.',
+				});
+			}
+			selectedComment;
+			if (selectedComment.userId === res.locals.user.id) {
+				selectedComment.destroy();
+				return res.redirect(`/api/post/${selectedComment.postId}`);
+			} else {
+				// 403 Forbidden  권한이 없을 때 사용
+				return res.status(403).render('blank', {
+					message: '댓글 삭제에 실패했습니다',
+				});
+			}
+		} catch {
+			return res.status(500).render('blank', {
+				message: '댓글 삭제에 실패했습니다. 관리자에게 문의 하십시오',
 			});
 		}
 	},
 );
+
 // // D delete
 
 module.exports = router;
